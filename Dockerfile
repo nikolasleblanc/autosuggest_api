61c190fc1723db416d56880808532bd73297c1fa
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2017-08-05 19:58:33 -0400

    Add readiness probe endpoint, tree

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ WORKDIR /usr/src/app
 COPY package.json /usr/src/app/
 COPY app.js /usr/src/app/
 
-RUN npm install -g nodemon
 RUN npm install
 
 EXPOSE 3000
--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 'use strict';
 
+const config = {};
+
 const app = require('express')();
 const redis = require("redis");
+const gcs = require('@google-cloud/storage')(config);
+const bucket = gcs.bucket('bestbuymulti');
+const pretree = require('trie-prefix-tree-serialize');
+
+let tree = pretree([]);
+
+let ready = false;
+
+const doReadBucket = () => {
+  const chunks = [];
+  bucket.file('output_trie.json').createReadStream()
+  //fs.createReadStream('output_trie.json')
+    .on('error', (err) => {
+      console.log('err', err);
+    })
+    .on('data', (data) => {
+      if (data !== '') {
+        chunks.push(data);
+      }
+    })
+    .on('end', function() {
+      const final = Buffer.concat(chunks);
+      tree.load(JSON.parse(final.toString()));
+      ready = true;
+    })
+}
 
 const slaveConfig = {
     host: '10.47.251.218',
@@ -21,18 +49,25 @@ const PORT = 3000;
 app.disable('x-powered-by');
 
 app.get('/', function (req, res) {
-    res.send({
-        message: 'Hello, World!'
-    });
+    res.send(process.env);
+});
+
+app.get('/_ready', function (req, res) {
+    ready ?
+        res.status(200) :
+        res.status(404)
 });
 
 app.get('/search/:str', function (req, res) {
+    const str = req.params.str;
     slave.get(req.params.str, function(err, reply) {
-        res.send({
-            message: 'You be searchin\', ' + req.params.str + ' ' + reply
-        });
-        // reply is null when the key is missing
-        console.log('err: ', err, 'reply: ', reply);
+        if (reply === null) {
+            const matches = tree.getPrefix(str)
+            master.set(str, matches);
+            res.send(matches);
+        } else {
+            res.send(reply);
+        }
     });
 });
 
