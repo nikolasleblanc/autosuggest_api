3069d2286f3e0975b08152e06498536706fac5d7
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2017-08-05 19:14:27 -0400

    See if I can get to redis

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const app = require('express')();
+const redis = require("redis");
+const client = redis.createClient({
+    host: '10.47.251.218',
+    port: '6379'
+});
 
 const PORT = 3000;
 
@@ -13,6 +18,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/search/:str', function (req, res) {
+    client.get(str, function(err, reply) {
+        // reply is null when the key is missing
+        console.log('err: ', err, 'reply: ', reply);
+    });
     res.send({
         message: 'You be searchin\', ' + req.params.str
     });
